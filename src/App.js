814b6e01f421b2f600f587fbe1eae8d2efefe5c7
Glatrix <73367967+Glatrix@users.noreply.github.com>
2023-02-19 11:23:55 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Monke Client</h1>
+      <h3>A very cool client for minecraft java</h3>
+    </div>
+  );
+}
+
+export default App;
